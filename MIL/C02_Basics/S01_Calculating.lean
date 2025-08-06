f4ff5b25f81b438b15c4d7757a4eda870bac9913
Darylgolden <darylgolden@gmail.com>
2025-08-06 15:44:15 +0800

    start

--- MIL/C02_Basics/S01_Calculating.lean
+++ MIL/C02_Basics/S01_Calculating.lean
@@ -7,10 +7,14 @@ example (a b c : ℝ) : a * b * c = b * (a * c) := by
 
 -- Try these.
 example (a b c : ℝ) : c * b * a = b * (a * c) := by
-  sorry
+  rw [mul_comm c b]
+  rw [mul_assoc b c a]
+  rw [mul_comm c a]
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  sorry
+  rw [← mul_assoc]
+  rw [mul_comm a b]
+  rw [mul_assoc]
 
 -- An example.
 example (a b c : ℝ) : a * b * c = b * c * a := by
